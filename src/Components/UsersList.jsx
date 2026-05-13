36f4935863d3344b8b50d7911b88fbfd714e06e6
kasarlaVivek <vivekkasarla781@gmail.com>
2026-05-13 12:02:16 +0530

    LATEST UPDATES

--- src/Components/UsersList.jsx
+++ src/Components/UsersList.jsx
@@ -11,7 +11,7 @@ function UsersList() {
     useEffect(() => {
         async function getUsers() {
             try {
-                let res = await fetch('http://localhost:3000/user-api/users', {
+                let res = await fetch('https://user-rest-api-1.onrender.com/user-api/users', {
                     method: "GET",
                 });
                 if (res.status === 201) {
@@ -35,15 +35,15 @@ function UsersList() {
         getUsers();
     }, [])
 
-    const goToUser = (userObj) =>{
-        navigate('/user',{state:userObj})
+    const goToUser = (userObj) => {
+        navigate('/user', { state: userObj })
     }
 
     return (
         <div className='max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-xl border border-gray-100'>
             <h1 className='text-2xl font-bold text-gray-800 mb-6 text-center'>List of Users</h1>
             {users.map((userObj, index) => (
-                <div key={userObj._id} className="border-2 py-2  cursor-pointer" onClick={()=>goToUser(userObj)}>
+                <div key={userObj._id} className="border-2 py-2  cursor-pointer" onClick={() => goToUser(userObj)}>
                     <p className="font-semibold text-gray-700">{userObj.name}</p>
                     <p className="text-sm text-gray-500">{userObj.email}</p>
                 </div>
